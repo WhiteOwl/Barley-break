7376a73d5f136dae26d5c6610a952c6a61f2fbe2
Daniel Kovarsky <daniel.kovarsky@anahoret.com>
2011-06-08 18:02:05 +0300

    Removed data methods to model

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,15 +1,6 @@
 class GameController < ApplicationController
-
   def index
-    
-    @a = Array.new
-    0.upto(14) do |t|
-      @a[t] = t + 1
-    end
-    @a = @a.shuffle
-    @a[15] = ""
-
-    
+      
     render "game"
   end
 end
