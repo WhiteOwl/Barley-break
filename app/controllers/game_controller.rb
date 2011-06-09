0d9bb0a622c8b90b73ecfa89d58c1214f85aee8a
Daniel Kovarsky <daniel.kovarsky@anahoret.com>
2011-06-09 18:03:00 +0300

    Added updare, create and show method. Changed index method.

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,6 +1,30 @@
 class GameController < ApplicationController
+
   def index
-      
-    render "game"
+
+
   end
+
+  def update
+    @game = Game.find(params[:id])
+    point = params[:point]
+    @game.move_it(point)
+   render "show"
+  end
+
+  def create
+    @game = Game.new
+    @game.fill_points
+    if @game.save
+      redirect_to game_path(@game.id)
+    else
+      render new_game_path
+    end
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+
 end
