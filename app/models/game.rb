475624dfca89e3baf03b1e32b4569621ae257e8d
Daniel Kovarsky <daniel.kovarsky@anahoret.com>
2011-06-09 18:04:12 +0300

    Added method for detect movable points and move them. Added before_filter for table filling.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,34 +1,55 @@
 class Game < ActiveRecord::Base
+  before_create :fill_points
   serialize :points, Array
-  
-  
-  
-  def fill_table
-    self.points = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, ""]
-    self.save
+
+
+
+  def fill_points
+
+    self.points = (1..15).to_a
+
+
+    self.points = self.points.shuffle
+    self.points[15] = ""
   end
-  
-  def self.is_movable?(t) 
-    if @f.points[t] == "[]"
-      
-       if @f.points[t-1] != nil
-        @f.points[t-1] = "yarr!"
-       end
-       
-       if @f.points[t+1] != nil
-         f.points[t+1] = "yarr!"
-       end
-       
-       if f.points[t-4] != nil
-         f.points[t-4] = "yarr!"
-       end
-       
-       if f.points[t+4] != nil
-         @f.points[t+4] = "yarr!"
-       end
+
+  def get_movable
+    movable_elements = Array.new
+
+    0.upto(15) do |t|
+      if self.points[t] == ""
+
+        if (((t-1) % 4 ) != 3) && self.points[t-1] != nil
+          movable_elements << t-1
+        end
+
+        if  (((t+1) % 4 ) != 0) && self.points[t+1] != nil
+          movable_elements << t+1
+        end
+
+        if (self.points[t-4] != nil)
+          movable_elements << t-4
+        end
+
+        if (self.points[t+4] != nil)
+          movable_elements << t+4
+        end
+
+      end
     end
+    return movable_elements
   end
-  
-  
-  
+
+  def movable?(point_number)
+    self.get_movable.include?(point_number)
+  end
+
+  def move_it(point_number)
+    self.points[point_number], self.points[empty_point_number] = self.points[empty_point_number], self.points[point_number]
+  end
+
+  def empty_point_number
+    self.points.index("")
+  end
+
 end
